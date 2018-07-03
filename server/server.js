e50646371bed241f2c9153dfb3b897b3c2bf90a2
Marcos <mallth194@gmail.com>
2018-07-02 21:24:08 -0400

    cambiando las salidas

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
  
 app.get('/', function (req, res) {
-  res.json('Hello World');
+  res.json('MI AMOR, TE AMO');
 });
  
 app.listen(PORT, () => { //la variable PORT viene del require('./config/config');
